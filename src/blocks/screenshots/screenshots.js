1c86c5a7fce3501706b394dfb8ac416ead5e4cda
Alexey <darth.tesla47@gmail.com>
2020-05-15 18:09:21 +0300

    made screenshot slider with slick

--- src/blocks/screenshots/screenshots.js
+++ src/blocks/screenshots/screenshots.js
@@ -6,8 +6,10 @@ $(".screenshots__slider").slick({
     "<div class='arrow-wrap slick-next'><i class='fas fa-long-arrow-alt-right'></i></div>",
   prevArrow:
     "<div class='arrow-wrap slick-prev'><i class='fas fa-long-arrow-alt-left'></i></div>",
-  slidesToShow: 3,
   centerMode: true,
-  initialSlide: 3,
   centerPadding: "50px",
+  focusOnSelect: true,
+  initialSlide: 3,
+  variableWidth: true,
+  infinite: false,
 });
