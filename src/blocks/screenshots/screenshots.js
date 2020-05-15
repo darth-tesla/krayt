d0b728a3f0b505222ad4fee6bb639c4b0202200c
Alexey <darth.tesla47@gmail.com>
2020-05-15 22:20:07 +0300

    add responsive for first slider

--- src/blocks/screenshots/screenshots.js
+++ src/blocks/screenshots/screenshots.js
@@ -3,17 +3,32 @@ import Swiper from "swiper";
 
 $(document).ready(function () {
   var mySwiper = new Swiper(".swiper-container-1", {
-    initialSlide: 3,
-    slidesPerView: 7,
+    slidesPerView: 1,
+    width: 190,
     centeredSlides: true,
-    width: 650,
-    containerModifierClass: "swiper-container-1"
-  });
-
-  $(".swiper-button-next").on("click", () => {
-    mySwiper.slideNext();
-  });
-  $(".swiper-button-prev").on("click", () => {
-    mySwiper.slidePrev();
+    initialSlide: 3,
+    containerModifierClass: "swiper-container-1",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+        width: 190,
+      },
+      768: {
+        slidesPerView: 3,
+        width: 400,
+      },
+      992: {
+        slidesPerView: 3,
+        width: 500,
+      },
+      1200: {
+        slidesPerView: 7,
+        width: 650,
+      },
+    },
   });
 });
