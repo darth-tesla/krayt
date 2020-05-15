cc73ee066ff6f5ea08521b535503c7ad85cecb2f
Alexey <darth.tesla47@gmail.com>
2020-05-15 21:09:38 +0300

    write top slider on swiper (need some fixes)

--- src/blocks/screenshots/screenshots.js
+++ src/blocks/screenshots/screenshots.js
@@ -1,15 +1,19 @@
 import * as $ from "jquery";
-import Slick from "slick-carousel";
+import Swiper from "swiper";
 
-$(".screenshots__slider").slick({
-  nextArrow:
-    "<div class='arrow-wrap slick-next'><i class='fas fa-long-arrow-alt-right'></i></div>",
-  prevArrow:
-    "<div class='arrow-wrap slick-prev'><i class='fas fa-long-arrow-alt-left'></i></div>",
-  centerMode: true,
-  centerPadding: "50px",
-  focusOnSelect: true,
-  initialSlide: 3,
-  variableWidth: true,
-  infinite: false,
+$(document).ready(function () {
+  var mySwiper = new Swiper(".swiper-container-1", {
+    initialSlide: 3,
+    slidesPerView: 7,
+    centeredSlides: true,
+    width: 650,
+    containerModifierClass: "swiper-container-1"
+  });
+
+  $(".swiper-button-next").on("click", () => {
+    mySwiper.slideNext();
+  });
+  $(".swiper-button-prev").on("click", () => {
+    mySwiper.slidePrev();
+  });
 });
